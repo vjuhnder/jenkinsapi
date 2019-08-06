c10fe860bbb5994d41354cb4c1bb2903dd157fb3
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-06 14:07:10 +0530

    added script for test

---
+++ QAfence.py
@@ -0,0 +1,9 @@
+from jenkinsapi.jenkins import Jenkins
+
+def get_server_instance():
+    jenkins_url = 'http://192,168.91.200:8080'
+    server = Jenkins(jenkins_url, username='vigneswaranjagadeesan _vigneswaran@uhnder.com_', password='JENKINSsiva@1')
+    return server
+
+if __name__ == '__main__':
+    print get_server_instance().version
